7775718071531228ed817cf80a5460d8a3f20815
gideonrynn <sarah.c.dillard@gmail.com>
2019-11-27 21:23:48 -0600

    setup forecast function that will pull second query, and set up first forecast card with blue background

--- assets/script.js
+++ assets/script.js
@@ -25,6 +25,17 @@ var cityWindSpeed = $('<div class=wind-speed>');
 var cityUV = $('<div class=uv-index>');
 
 cityDetails.append(cityName, cityTemp, cityHumid, cityWindSpeed, cityUV);
+
+
+var fiveDayCityForecast = $('.city-forecast');
+
+var fiveDayContainer = $('<div class=five-container>');
+var fiveDayTime = $('<div class=card-text id=five-date>');
+var fiveDayTemp = $('<div class=card-text id=five-temp>');
+var fiveDayHumid = $('<div class=card-text id=five-humid>');
+
+fiveDayContainer.append(fiveDayTime, fiveDayTemp, fiveDayHumid);
+fiveDayCityForecast.append(fiveDayContainer);
  
  ///////////////////
  ///////API calls////
@@ -38,6 +49,12 @@ cityDetails.append(cityName, cityTemp, cityHumid, cityWindSpeed, cityUV);
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +
       "Chicago" + "&units=imperial&appid=" + APIKey;
 
+    console.log(queryURL);
+
+    var queryForecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=Chicago&units=imperial&appid=3ccebe214d7b6f05e838f63e5034dcde";
+
+    console.log("This is the 5 day Forecast " + queryForecastURL);
+
 
       // cityUV.text("UV Index " + response.)
 
@@ -47,6 +64,7 @@ cityDetails.append(cityName, cityTemp, cityHumid, cityWindSpeed, cityUV);
 
         function searchCity () {
           
+          //prevent button from triggering default page refresh
           event.preventDefault();
           
             var citySearch = $('#search').val();
@@ -73,6 +91,7 @@ cityDetails.append(cityName, cityTemp, cityHumid, cityWindSpeed, cityUV);
       // We store all of the retrieved data inside of an object called "response"
       .then(function(response) {
 
+        console.log("This is the first query response " + response);
         // Transfer content to HTML
         cityName.html("<h1>" + response.name);
         cityWindSpeed.text("Wind Speed: " + response.wind.speed);
@@ -87,7 +106,33 @@ cityDetails.append(cityName, cityTemp, cityHumid, cityWindSpeed, cityUV);
 
 }
 
-runQuery ();
-
-
 //at start, the run ajax call that will display city details
+runQuery ();
+runForecastQuery ();
+
+
+function runForecastQuery () {
+
+  $.ajax({
+    url: queryForecastURL,
+    method: "GET"
+  })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function(forecast) {
+
+      console.log(forecast);
+
+      // Transfer content to HTML
+      fiveDayTime.html(forecast.list[0].dt_txt);
+      fiveDayTemp.text("Temperature (F) " + forecast.list[0].main.temp);
+      fiveDayHumid.text("Humidity: " + forecast.list[0].main.humidity);
+
+      // Converts the temp to Kelvin with the below formula
+      var tempF = (forecast.list[0].main.temp - 273.15) * 1.80 + 32;
+      $(".tempF").text("Temperature (Kelvin) " + tempF);
+
+  });
+}
+
+
+
