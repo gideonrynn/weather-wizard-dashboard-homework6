f935d258146205ba26654cdc23c9cb44dcb8eeb0
gideonrynn <sarah.c.dillard@gmail.com>
2019-11-27 22:55:49 -0600

    updated forecast function to create multiple divs to hold 5-day data

--- assets/script.js
+++ assets/script.js
@@ -29,13 +29,7 @@ cityDetails.append(cityName, cityTemp, cityHumid, cityWindSpeed, cityUV);
 
 var fiveDayCityForecast = $('.city-forecast');
 
-var fiveDayContainer = $('<div class=five-container>');
-var fiveDayTime = $('<div class=card-text id=five-date>');
-var fiveDayTemp = $('<div class=card-text id=five-temp>');
-var fiveDayHumid = $('<div class=card-text id=five-humid>');
-
-fiveDayContainer.append(fiveDayTime, fiveDayTemp, fiveDayHumid);
-fiveDayCityForecast.append(fiveDayContainer);
+var citySearch = $('#search').val();
  
  ///////////////////
  ///////API calls////
@@ -51,7 +45,8 @@ fiveDayCityForecast.append(fiveDayContainer);
 
     console.log(queryURL);
 
-    var queryForecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=Chicago&units=imperial&appid=3ccebe214d7b6f05e838f63e5034dcde";
+    // Set url that will query the database. default is chicago
+    var queryForecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + "Chicago" + "&units=imperial&appid=" + APIKey;
 
     console.log("This is the 5 day Forecast " + queryForecastURL);
 
@@ -67,13 +62,16 @@ fiveDayCityForecast.append(fiveDayContainer);
           //prevent button from triggering default page refresh
           event.preventDefault();
           
-            var citySearch = $('#search').val();
+            
             console.log(citySearch);
 
             queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +
             citySearch + "&units=imperial&appid=" + APIKey;
 
+            queryForecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&units=imperial&appid=" + APIKey;
+
             runQuery ();
+            runForecastQuery ();
 
             $('.list-group').append('<li class=list-group-item>' + citySearch + '</li>');
 
@@ -95,7 +93,7 @@ fiveDayCityForecast.append(fiveDayContainer);
         // Transfer content to HTML
         cityName.html("<h1>" + response.name);
         cityWindSpeed.text("Wind Speed: " + response.wind.speed);
-        cityHumid.text("Humidity: " + response.main.humidity);
+        cityHumid.text("Humidity: " + response.main.humidity + '%');
         cityTemp.text("Temperature (F) " + response.main.temp);
 
         // Converts the temp to Kelvin with the below formula
@@ -106,32 +104,53 @@ fiveDayCityForecast.append(fiveDayContainer);
 
 }
 
-//at start, the run ajax call that will display city details
+//at start, the run ajax call that will display city current details and forecast details
 runQuery ();
 runForecastQuery ();
 
 
 function runForecastQuery () {
 
-  $.ajax({
-    url: queryForecastURL,
-    method: "GET"
-  })
+ 
+    $.ajax({
+      url: queryForecastURL,
+      method: "GET"
+    })
     // We store all of the retrieved data inside of an object called "response"
     .then(function(forecast) {
 
       console.log(forecast);
 
-      // Transfer content to HTML
-      fiveDayTime.html(forecast.list[0].dt_txt);
-      fiveDayTemp.text("Temperature (F) " + forecast.list[0].main.temp);
-      fiveDayHumid.text("Humidity: " + forecast.list[0].main.humidity);
+      //works when each of the list items contains an i, along with container and variables - also 
+     for (var i = 0; i < 5; i++) {
 
-      // Converts the temp to Kelvin with the below formula
-      var tempF = (forecast.list[0].main.temp - 273.15) * 1.80 + 32;
-      $(".tempF").text("Temperature (Kelvin) " + tempF);
+      var fiveDayContainer = $('<div class=five-container>');
+
+      var fiveDayTime = $('<div class=card-text id=five-date>');
+
+      var fiveDayTemp = $('<div class=card-text id=five-temp>');
+
+      var fiveDayHumid = $('<div class=card-text id=five-humid>');
+
+      // Transfer content to HTML
+      fiveDayTime.html(forecast.list[i].dt_txt);
+      fiveDayTemp.text("Temperature (F) " + forecast.list[i].main.temp);
+      fiveDayHumid.text("Humidity: " + forecast.list[i].main.humidity + "%");
+
+      
+      fiveDayCityForecast.append(fiveDayContainer);
+      fiveDayContainer.append(fiveDayTime, fiveDayTemp, fiveDayHumid);
+
+      // // Converts the temp to Kelvin with the below formula
+      // var tempF = (forecast.list[i].main.temp - 273.15) * 1.80 + 32;
+      // $(".tempF").text("Temperature (Kelvin) " + tempF);
+
+      console.log('done');
+    }
 
   });
+  
+
 }
 
 
