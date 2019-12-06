68959d65870f89ac7379a80d614a94b475d1a8b5
gideonrynn <sarah.c.dillard@gmail.com>
2019-12-06 11:08:44 -0600

    cleaned up code for what displays by default on the page when no cities have yet been searched, and updated Readme file.

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,7 @@
  /////////////////////
 
 var cityDetails = $('.city-details');
+var cityIconDetails = $('.icon-details');
 
 //create variables for primary city display divs and append to 
 var cityName = $('<h5 class=city-name></h5>');
@@ -14,7 +15,7 @@ var cityUV = $('<div class=uv-index>');
 var cityTime = $('<span class=city-time>');
 var cityIcon = $('<img class=city-icon>');
 
-cityDetails.append(cityName, cityTime, cityTemp, cityHumid, cityWindSpeed, cityUV, cityIcon);
+cityDetails.append(cityName, cityTime, cityIcon, cityTemp, cityHumid, cityWindSpeed, cityUV);
 
 //set var for div into which forecast cards and details will be pulled
 var fiveDayCityForecast = $('.card-group');
@@ -26,23 +27,19 @@ var currentDate = moment().format('MM-DD-YYYY');
  ///Startup API queries and info////
  ///////////////////////////////////
 
- //used a default value (Chicago) in the case that the geolocation does not pull latitude and longitude from the user
-var startValue = "&lat=41.88&lon=-87.62";
+ 
 
- //use geolocation to prompt for and pull user latitude and longtiude information.
+ // This is my API key/parameter
+ var APIKey = "&appid=3ccebe214d7b6f05e838f63e5034dcde";
 
-// This is my API key/parameter
-var APIKey = "&appid=3ccebe214d7b6f05e838f63e5034dcde";
+ //This variable will contain the user input and starts off as empty
+ var citySearch = "";
 
-// Set url that will query the database. default is chicago
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +
-  startValue + "&units=imperial" + APIKey;
+ console.log(citySearch == "");
 
-var queryForecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + startValue + "&units=imperial" + APIKey;
+//  if (citySearch == "") {
 
-var queryUvURL = "https://api.openweathermap.org/data/2.5/uvi?" + APIKey + startValue;
 
-var citySearch = "";
  //////////////////////////////////
  //When Submit Button Is Clicked///
  //////////////////////////////////
@@ -56,14 +53,16 @@ var citySearch = "";
 
     citySearch = $('#search').val();
     searchCity ();
+    $('.list-group').append('<li class=list-group-item>' + '<a href=#>' + citySearch + '</a>' + '</li>');
+    addInputArray ();
   });
 
-  //does not work currently - meant to grab the value of the div and pull into the citySearch variable to run the searchCity function
-  // $('li').on("click", function () {
-  //   citySearch = $(this).val();
-  //   console.log(citySearch);
-  //   searchCity ();
-  // })
+  // does not work currently - meant to grab the value of the div and pull into the citySearch variable to run the searchCity function
+  $('li').on("click", function () {
+    citySearch = $(this).val();
+    console.log(citySearch);
+    searchCity ();
+  })
         
   //////////////
  //Functions///
@@ -71,11 +70,10 @@ var citySearch = "";
 
     function searchCity () {
 
-
-    //clear any existing divs
+      //clear any existing divs
       fiveDayCityForecast.empty();
 
-            
+      //set value of queryURL 
       queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&units=imperial" + APIKey;
 
       queryForecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&units=imperial" + APIKey;
@@ -83,10 +81,7 @@ var citySearch = "";
       queryUV ();
       runQuery ();
       runForecastQuery ();
-        
-      $('.list-group').append('<li class=list-group-item>' + '<a href=#>' + citySearch + '</a>' + '</li>');
 
-      addInputArray();
     }
 
     //create function that will run ajax call and update city details
@@ -172,11 +167,10 @@ function runForecastQuery () {
      //Store retrieved data inside response object
     .then(function(forecast) {
     
-    //works when each of the list items contains an i
+    
      for (var i = 1; i < 40; i++) {
 
       //defining all of the date elements that will be used to match against the forecast object date
-
       var fromNowDate = moment().add(1, 'days').format('YYYY-MM-DD');
       var fromNowDateTwo = moment().add(2, 'days').format('YYYY-MM-DD');
       var fromNowDateThree = moment().add(3, 'days').format('YYYY-MM-DD');
@@ -258,33 +252,33 @@ function runForecastQuery () {
 var cityList = [];
 
 console.log(cityList);
-//on click, call function which processes through each text area, grabs the value, and pushes it to the array
+
+//on click, run function which processes through each text area, grabs the value, and pushes it to the array
 function addInputArray () {
 
   if (cityList === null) {
     cityList = [];
   }
   
-
+  //for each value entered in the search,
   $("#search").each( function() {
+
      var cityNameSearched = $(this).val();
 
-     if (cityNameSearched !== null) {
-      cityList.push({input: cityNameSearched});
-     }
-     else {
-       cityList.push({citySearch});
-     }
-     
+     cityList.push({input: cityNameSearched});
+
   })
 
   //run saveToLocal function below
   saveToLocalStorage();
+
 }
 
 //save list of inputs to localStorage
 function saveToLocalStorage () {     
+
   var str = JSON.stringify(cityList);
+
   localStorage.setItem('cityList', str);
   
 }
@@ -294,26 +288,34 @@ function getFromLocal () {
 
   cityList = JSON.parse(localStorage.getItem('cityList'));
 
-  if (cityList !== null) {
-  for (var i = 0; i < cityList.length; i++) {
+
+  if (cityList !== "" && cityList !== null) {
+
+    for (var i = 0; i < cityList.length; i++) {
+
       var cities = cityList;
       console.log(cities);
-     
+      
       li = $('<li class=list-group-item>' + '<a href=#>' + cities[i].input + '</a>' + '</li>');
       $(".list-group").append(li);
 
-      }
-      //get value of last city searched by user in the cityList array, so that it will be displayed on the homepage by default
-      lastCitySearch = cityList[cityList.length - 1];
-      citySearch = Object.values(lastCitySearch);
-      console.log(citySearch);
+    }
+    
+    //get value of last city searched by user in the cityList array, so that it will be displayed on the homepage by default
+    var lastCitySearch = cityList[cityList.length - 1];
+    citySearch = Object.values(lastCitySearch);
+    console.log(citySearch);
 
-      searchCity ();
+    searchCity ();
+
   }
+
   else {
     citySearch = "Chicago";
+
     searchCity ();
   }
+
 }
 
 //at start, the run ajax call that will display city current details and forecast details
